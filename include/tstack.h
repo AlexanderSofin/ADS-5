a075b5041577f783c91899dddc54f4ff61315c27
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-04-02 20:14:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -11,15 +11,15 @@ class TStack {
     TStack() : top(-1) { }
     T get() const {
       return arr[top];
-	  }
+          }
     bool isEmpty() const {
       return top == -1;
     }
     void pop() {
       if (top >= 0) top--;
-	  }
-	  void push(T item) {
-		  if (top < size - 1) arr[++top] = item;
-	  }
+          }
+          void push(T item) {
+                  if (top < size - 1) arr[++top] = item;
+          }
 };
 #endif  // INCLUDE_TSTACK_H_
