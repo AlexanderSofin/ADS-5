64d75545d69e019ca4ad6f9f43f0e8d74e95e895
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-04-02 19:50:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,21 +5,21 @@
 template<typename T, int size>
 class TStack {
    private:
-   T arr[100];
-   int top;
+     T arr[100];
+     int top;
    public:
-   TStack() : top(-1) { }
-   T get() const {
-   return arr[top];
-   }
+     TStack() : top(-1) { }
+     T get() const {
+       return arr[top];
+     }
    bool isEmpty() const {
-   return top == -1;
+     return top == -1;
    }
    void pop() {
-   if (top >= 0) top--;
+     if (top >= 0) top--;
    }
    void push(T item) {
-   if (top < size - 1) arr[++top] = item;
+     if (top < size - 1) arr[++top] = item;
    }
 };
 #endif  // INCLUDE_TSTACK_H_
