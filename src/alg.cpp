52b3765579e3f9ece2a9699731632b4ba28d0226
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-04-02 19:18:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,33 +1,40 @@
-// Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
+#include <iostream>
 #include "tstack.h"
 using namespace std;
-int prior(char ch) {
-    switch (ch) {
-        case '(': return 0;
-        case ')': return 1;
-        case '+': return 2;
-        case '-': return 2;
-        case '*': return 3;
-        case '/': return 3;
-        default: return -1;
+
+int prior(char ch)
+{
+    switch (ch)
+    {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default: return -1;
     }
 }
-string infx2pstfx(string inf) {
+
+string infx2pstfx(string inf)
+{
     TStack<char> stack1;
     string tmp = "";
-    for (int i = 0; i < inf.size(); i++) {
+    for (int i = 0; i < inf.size(); i++)
+    {
         char ch = inf[i];
         int k = prior(ch);
+
         if (k == -1)
             tmp.append(string(1, ch));
-        else {
+        else
             if (stack1.isEmpty() || k == 0 || k > prior(stack1.get()))
                 stack1.push(ch);
-            else {
-                if (ch == ')') {
-                    while (true) {
+            else
+            {
+                if (ch == ')')
+                    while (true)
+                    {
                         char sym = stack1.get();
                         stack1.pop();
                         if (sym != '(')
@@ -35,9 +42,10 @@ string infx2pstfx(string inf) {
                         else
                             break;
                     }
-                }
-                else {
-                    while (!stack1.isEmpty()) {
+                else
+                {
+                    while (!stack1.isEmpty())
+                    {
                         char lastStackEl = stack1.get();
                         stack1.pop();
                         if (prior(lastStackEl) >= k)
@@ -47,15 +55,19 @@ string infx2pstfx(string inf) {
                 }
             }
     }
-    while (!stack1.isEmpty()) {
+    while (!stack1.isEmpty())
+    {
         char lastStackEl = stack1.get();
         stack1.pop();
         tmp.append(string(1, lastStackEl));
     }
     return tmp;
 }
-int excute_calc(int k1, int k2, char pst) {
-    switch (pst) {
+
+int excute_calc(int k1, int k2, char pst)
+{
+    switch (pst)
+    {
     case '+': return k1 + k2;
     case '-': return k1 - k2;
     case '*': return k1 * k2;
@@ -63,21 +75,29 @@ int excute_calc(int k1, int k2, char pst) {
     default: return -1;
     }
 }
-int eval(string pref) {
+
+int eval(string pst)
+{
     TStack<int> stack2;
-    for (int i = 0; i < pst.size(); i++) {
+    for (int i = 0; i < pst.size(); i++)
+    {
         char ch = pst[i];
         int priority = prior(ch);
+
         if (priority == -1)
             stack2.push(ch - 48);
-        else {
+        else
+        {
             int  k1 = stack2.get();
             stack2.pop();
+
             int k2 = stack2.get();
             stack2.pop();
+
             int res = excute_calc(k2, k1, ch);
             stack2.push(res);
         }
+
     }
     return stack2.get();
 }
